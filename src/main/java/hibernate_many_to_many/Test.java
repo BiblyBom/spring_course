76c6f9d402eb0089906bbfd86004248d7cfb8937
Maxim Boyev <maximboev90@gmail.com>
2022-08-12 19:46:51 +0300

    В пакете hibernate_many_to_many в классах Test, Child, Section проведены изменения в соответствии с курсом

--- src/main/java/hibernate_many_to_many/Test.java
+++ src/main/java/hibernate_many_to_many/Test.java
@@ -20,10 +20,10 @@ public class Test {
         try {
 //            session = factory.getCurrentSession();
 //
-//            Section section1 = new Section("Football");
-//            Child child1 = new Child("Maxim", 5);
-//            Child child2 = new Child("Masha", 7);
-//            Child child3 = new Child("Andrey", 6);
+//            Section section1 = new Section("Psychology");
+//            Child child1 = new Child("Anna", 11);
+//            Child child2 = new Child("Oleg", 10);
+//            Child child3 = new Child("Katerina", 12);
 //
 //            section1.addChildToSection(child1);
 //            section1.addChildToSection(child2);
@@ -31,7 +31,7 @@ public class Test {
 //
 //            session.beginTransaction();
 //
-//            session.save(section1);
+//            session.persist(section1);
 //
 //            session.getTransaction().commit();
 //            System.out.println("Done!");
@@ -57,14 +57,26 @@ public class Test {
 //            System.out.println("Done!");
 
             // ******************************************************
+//            session = factory.getCurrentSession();
+//
+//
+//            session.beginTransaction();
+//
+//            Child child = session.get(Child.class, 4);
+//            System.out.println(child);
+//            System.out.println(child.getSections());
+//
+//            session.getTransaction().commit();
+//            System.out.println("Done!");
+            // ****************************************************
             session = factory.getCurrentSession();
 
 
             session.beginTransaction();
 
-            Child child = session.get(Child.class, 4);
-            System.out.println(child);
-            System.out.println(child.getSections());
+            Section section = session.get(Section.class, 7);
+
+            session.delete(section);
 
             session.getTransaction().commit();
             System.out.println("Done!");
